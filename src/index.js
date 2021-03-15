c66b8a923a72eb37b59ca343751e962835b823e8
Alina-Fuchs <kuzmenkoalina89@gmail.com>
2021-03-15 16:30:26 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, input) {
+  let brackets = "[]{}()"
+  let stack = []
+
+  for(let bracket of input) {
+    let bracketsIndex = brackets.indexOf(bracket)
+
+    if (bracketsIndex === -1){
+      continue
+    }
+
+    if(bracketsIndex % 2 === 0) {
+      stack.push(bracketsIndex + 1)
+    } else {
+      if(stack.length === 0 || stack.pop() !== bracketsIndex) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0
 }
